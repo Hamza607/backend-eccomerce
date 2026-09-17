e765826c0d7dba87fc703cb4820e981f8ddc2933
Hamza607 <101787339+Hamza607@users.noreply.github.com>
2026-09-17 13:06:59 +0500

    fixed for production

--- api/index.js
+++ api/index.js
@@ -6,21 +6,12 @@ const connectDB = require("../src/config/db");
 let isConnected = false;
 
 const handler = async (req, res) => {
-  try {
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-    }
-
-    return app(req, res);
-  } catch (error) {
-    console.error("SERVERLESS ERROR:", error);
-
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-    });
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
   }
+
+  return app(req, res);
 };
 
 module.exports = handler;
