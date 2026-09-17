83efd18a517f63004129f959dc4de296699be329
Hamza607 <101787339+Hamza607@users.noreply.github.com>
2026-09-17 14:56:28 +0500

    fixed logger issue

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -4,20 +4,10 @@ const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json(),
+    winston.format.json()
   ),
-
-  transport: [
+  transports: [
     new winston.transports.Console(),
-
-    new winston.transports.File({
-      filename: "logs/error.log",
-      level: "error",
-    }),
-
-    new winston.transports.File({
-      filename: "logs/combined.log",
-    }),
   ],
 });
 
